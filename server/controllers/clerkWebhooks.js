498b331e4233fa7b7a9bc1798af6f2cf78aacb57
Krishna Aman <krishnaaman432@gmail.com>
2025-09-22 00:19:36 +0530

    Fix Clerk webhook for Vercel deployment

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,56 +1,61 @@
 import User from "../models/User.js";
-
 import { Webhook } from "svix";
+import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI;
 
-
-const clerkWebHooks = async(req,res)=>{
-    try{
-        //create a svix instance with clerk webhook secret
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        //getting headers
-        const headers = {
-
-            "svix-id" : req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-
-        }
-
-        await whook.verify(JSON.stringify(req.body),headers)
-
-        const {data,type} = req.body
-        const userData = {
-            _id:data.id,
-            email:data.email_addresses[0].email_address,
-            username:data.first_name + " " + data.last_name,
-            image:data.image_url,
-        }
-        //switch case for diff events
-        switch (type) {
-            case "user.created": 
-            await User.create(userData);
-                
-                break;
-            case "user.updated": 
-            await User.findByIdAndUpdate(data.id,userData);
-                
-                break;
-            case "user.deleted": 
-            await User.findByIdAndUpdate(data.id);
-                
-                break;
-            
-            default:
-                break;
-        }
-        res.json({success:true,message:"Webhook Recieved"})
-    }catch(error){
-      console.log(error.message);
-      res.json({success:false, message:error.message})
-      
-
-    }
+// Connect to DB if not connected
+async function connectDB() {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(MONGO_URI);
+    console.log("DB connected");
+  }
 }
 
-export default clerkWebHooks;
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ success: false, message: "Method not allowed" });
+  }
+
+  try {
+    await connectDB();
+
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
+
+    await whook.verify(JSON.stringify(req.body), headers);
+
+    const { data, type } = req.body;
+
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0]?.email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
+
+    switch (type) {
+      case "user.created":
+        await User.create(userData);
+        break;
+      case "user.updated":
+        await User.findByIdAndUpdate(data.id, userData);
+        break;
+      case "user.deleted":
+        await User.findByIdAndDelete(data.id);
+        break;
+      default:
+        break;
+    }
+
+    res.status(200).json({ success: true, message: "Webhook received" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+}
