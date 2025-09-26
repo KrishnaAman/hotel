ba12463e94e1fc5f895ad3a151ab1e3cd61744e7
Krishna Aman <krishnaaman432@gmail.com>
2025-09-26 23:26:57 +0530

    ,

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,12 +1,21 @@
 import User from "../models/User.js";
 import { Webhook } from "svix";
+import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI;
 
+async function connectDB() {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(MONGO_URI);
+    console.log("DB connected");
+  }
+}
 
-const clerkWebHooks = async (req,res)=>{
+const clerkWebHooks = async (req, res) => {
+  if (req.method !== "POST") return res.status(405).json({ success: false, message: "Method not allowed" });
 
   try {
-  
+    await connectDB();
 
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
@@ -15,46 +24,41 @@ const clerkWebHooks = async (req,res)=>{
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     };
-  
-    
+
     await whook.verify(JSON.stringify(req.body), headers);
-    
+
     const { data, type } = req.body;
-     const userData = {
-      _id: data.id,
-      email: data.email_addresses[0]?.email_address,
+
+    const userData = {
+      clerkId: data.id,
+      email: data.email_addresses?.[0]?.email_address,
       username: data.first_name + " " + data.last_name,
       image: data.image_url,
     };
-    
+
     switch (type) {
       case "user.created":
-        await User.create(userData);
+        await User.findOneAndUpdate({ clerkId: data.id }, userData, { upsert: true });
         break;
+
       case "user.updated":
-        await User.findByIdAndUpdate(data.id, userData);
+        await User.findOneAndUpdate({ clerkId: data.id }, userData);
         break;
+
       case "user.deleted":
-        await User.findByIdAndDelete(data.id);
+        const deletedUser = await User.findOneAndDelete({ clerkId: data.id });
+        console.log("Deleted user:", deletedUser);
         break;
+
       default:
         break;
     }
-    res.json({success:true,message:"WebHook Recieved"})
-    
+
+    res.status(200).json({ success: true, message: "Webhook received" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: error.message });
   }
-    catch(error){
-      console.log(error.message);
-      res.json({success:false,message:error.message})
-      
+};
 
-    }
-  }
-  export default clerkWebHooks;
-
-
-
-   
-
-
-   
+export default clerkWebHooks;
--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
+    clerkId: { type: String, required: true, unique: true },
     _id: {type: String, required:true},
     username: {type: String, required:true},
     email: {type: String, required:true},
